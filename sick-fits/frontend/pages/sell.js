0e79e99a629481b999204daf1770f7b6dfdf0fa4
Ryan Walker <rytwalker@gmail.com>
2019-08-19 16:05:21 -0700

    gettin started

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Sell = () => {
+  return (
+    <div>
+      <p>Hey Sell things!</p>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default Sell;
