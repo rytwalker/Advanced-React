72898d0a02c5b86a596d5d5e748b3baf721e0b4b
Ryan Walker <rytwalker@gmail.com>
2019-08-19 17:04:29 -0700

    basic styling done

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,19 +1,74 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightgrey: '#e1e1e1',
+  offwhite: '#ededed',
+  maxwidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0.09)'
+};
+
+injectGlobal`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2') 
+        format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+    html {
+        box-sizing: border-box;
+        font-size: 10px;
+    }
+
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        font-size: 1.5rem;
+        line-height: 2;
+        font-family: 'radnika_next';
+    }
+
+    a {
+        text-decoration: none;
+        color: ${theme.black};
+    }
+`;
 
 class Page extends Component {
   state = {};
   render() {
     const { children } = this.props;
     return (
-      <div>
-        <Meta />
-        <Header />
-        {children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxwidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default Page;
